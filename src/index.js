d39aec02f54d4c4e5308049cfb85eb661c852c7c
fiedri <ruizlopezfriedrichjavier@gmail.com>
2026-02-05 18:38:48 -0400

    Modificacion del mensaje

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { connectDB } from "./users.js";
 import express from 'express';
 const app = express();
 
-app.get('/', (req, res) => res.send('Bot de Fiedri en línea 🚀'));
+app.get('/', (req, res) => res.status(200).send('OK'););
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
