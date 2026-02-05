2cb03d7dcc3a18d4d93e653a12d8478032624b56
fiedri <ruizlopezfriedrichjavier@gmail.com>
2026-02-05 18:40:36 -0400

    Modificacion de mensaje del endpoint

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { connectDB } from "./users.js";
 import express from 'express';
 const app = express();
 
-app.get('/', (req, res) => res.status(200).send('OK'););
+app.get('/', (req, res) => res.status(200).send('OK'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
